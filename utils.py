530a704ff43843572c683a691b1431d38364215e
cylof22 <cylof22@gmail.com>
2018-05-30 21:32:12 +0800

    fix imread error

--- utils.py
+++ utils.py
@@ -8,8 +8,11 @@ import scipy.misc
 import numpy as np
 import copy
 
-from imageio import imread as _imread
-from imageio import imwrite
+try:
+    _imread = scipy.misc.imread
+except AttributeError:
+    from imageio import imread as _imread
+
 
 pp = pprint.PrettyPrinter()
 
@@ -43,34 +46,9 @@ class ImagePool(object):
 
 def load_test_data(image_path, fine_width=256, fine_height=256):
     img = imread(image_path)
-    img = scipy.misc.imresize(img, [fine_width, fine_height])
+    img = scipy.misc.imresize(img, [fine_width, fine_height, 3])
     img = img/127.5 - 1
     return img
-
-def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
-    img_A = imread(image_path[0])
-    img_B = imread(image_path[1])
-    if not is_testing:
-        img_A = scipy.misc.imresize(img_A, [load_size, load_size])
-        img_B = scipy.misc.imresize(img_B, [load_size, load_size])
-        h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
-        img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
-
-        if np.random.random() > 0.5:
-            img_A = np.fliplr(img_A)
-            img_B = np.fliplr(img_B)
-    else:
-        img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
-        img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
-
-    img_A = img_A/127.5 - 1.
-    img_B = img_B/127.5 - 1.
-
-    img_AB = np.concatenate((img_A, img_B), axis=2)
-    # img_AB shape: (fine_size, fine_size, input_c_dim + output_c_dim)
-    return img_AB
 
 # -----------------------------
 
@@ -102,7 +80,7 @@ def merge(images, size):
 def imsave(images, size, path):
     #Todo: restore the quality to 100% for jpeg
     kwargs_write = {'quality':100}
-    imwrite(path, merge(images, size), kwargs_write)
+    scipy.misc.imsave(path, merge(images, size))
 
 def center_crop(x, crop_h, crop_w,
                 resize_h=64, resize_w=64):
